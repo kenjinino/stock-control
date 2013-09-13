5daed7715b83e765938a64825afcaced98228d77
Edson Ninomiya <kenjinino07@gmail.com>
2013-09-13 01:24:30 -0300

    Orders eager loading

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -67,7 +67,7 @@ class OrdersController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_order
-      @order = Order.find(params[:id])
+      @order = Order.includes(order_lines: [:product]).find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
